d3429cc0ec23c986be376d4adc0979c53f33f169
Jake Luer <jake.luer@gmail.com>
2012-10-03 10:43:48 -0400

    test for reader dsl referenced length

--- test/reader_dsl.js
+++ test/reader_dsl.js
@@ -2,28 +2,65 @@
 describe('Reader DSL', function () {
   var Buffers = require('bufs');
 
-  it('can take from the buffer', function (done) {
-    var bufs = new Buffers()
-      , reader = lotus.reader();
+  describe('take', function () {
 
-    reader.take(3, 'token', function (res) {
-      res.should.be.instanceof(Buffer)
-        .with.length(3)
-        .and.deep.equal(new Buffer([ 1, 2, 3 ]));
-      return '123';
+    it('can take a numbered length', function (done) {
+      var bufs = new Buffers()
+        , reader = lotus.reader();
+
+      reader.take(3, 'token', function (res) {
+        res.should.be.instanceof(Buffer)
+          .with.length(3)
+          .and.deep.equal(new Buffer([ 1, 2, 3 ]));
+        return '123';
+      });
+
+      reader.handle(bufs, function (err, msg) {
+        should.not.exist(err);
+        msg.should.deep.equal({ token: '123' });
+        done();
+      });
+
+      setTimeout(function () {
+        bufs.push(new Buffer([ 1 ]));
+        bufs.push(new Buffer([ 2 ]));
+        bufs.push(new Buffer([ 3 ]));
+      }, 10);
     });
 
-    reader.handle(bufs, function (err, msg) {
-      should.not.exist(err);
-      msg.should.deep.equal({ token: '123' });
-      done();
+    it('can take a referenced length', function (done) {
+      var bufs = new Buffers()
+        , reader = lotus.reader();
+
+      reader.take(0, 'tokenLen', function (res) {
+        res.should.be.instanceof(Buffer)
+          .with.length(0);
+        return 3;
+      });
+
+      reader.take('tokenLen', 'token', function (res) {
+        res.should.be.instanceof(Buffer)
+          .with.length(3)
+          .and.deep.equal(new Buffer([ 1, 2, 3 ]));
+        return '123';
+      });
+
+      reader.handle(bufs, function (err, msg) {
+        should.not.exist(err);
+        msg.should.deep.equal({
+            tokenLen: 3
+          , token: '123'
+        });
+        done();
+      });
+
+      setTimeout(function () {
+        bufs.push(new Buffer([ 1 ]));
+        bufs.push(new Buffer([ 2 ]));
+        bufs.push(new Buffer([ 3 ]));
+      }, 10);
     });
 
-    setTimeout(function () {
-      bufs.push(new Buffer([ 1 ]));
-      bufs.push(new Buffer([ 2 ]));
-      bufs.push(new Buffer([ 3 ]));
-    }, 10);
   });
 
 });
